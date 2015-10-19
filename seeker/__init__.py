612c003b395023cb47fdd169e99bb7309949046d
Dan Watson <watsond@imsweb.com>
2015-10-19 15:05:37 -0400

    Expose seeker.DEFAULT_ANALYZER

--- seeker/__init__.py
+++ seeker/__init__.py
@@ -1,6 +1,6 @@
 __version__ = '0.4.0'
 
-from .mapping import Indexable, ModelIndex, document_from_model, build_mapping, document_field, deep_field_factory, RawString, RawMultiString
+from .mapping import Indexable, ModelIndex, document_from_model, build_mapping, document_field, deep_field_factory, RawString, RawMultiString, DEFAULT_ANALYZER
 from .facets import Facet, TermsFacet, GlobalTermsFacet, YearHistogram, RangeFilter
 from .views import SeekerView, Column
 from .utils import search, index
