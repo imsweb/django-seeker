536b6cbb06ec31e1d157dc5d2109a9aa0d4fce09
Dan Watson <watsond@imsweb.com>
2016-03-22 15:00:46 -0400

    Don't send _id twice when indexing

--- seeker/__init__.py
+++ seeker/__init__.py
@@ -3,7 +3,7 @@ __version__ = '3.0-dev'
 from .mapping import Indexable, ModelIndex, document_from_model, build_mapping, document_field, deep_field_factory, RawString, RawMultiString, DEFAULT_ANALYZER
 from .facets import Facet, TermsFacet, GlobalTermsFacet, YearHistogram, RangeFilter
 from .views import SeekerView, Column
-from .utils import search, index
+from .utils import search, index, delete
 from .registry import register, documents, model_documents, app_documents
 
 default_app_config = 'seeker.apps.SeekerConfig'
