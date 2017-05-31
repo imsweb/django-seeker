549f2ef3132a99ac02f97c9bba8aa148fc91b5e0
Chris Wassenius <wassenic@imsweb.com>
2017-05-31 11:22:30 -0400

    swap duplicate post_clear for post_remove

--- seeker/indexer.py
+++ seeker/indexer.py
@@ -36,7 +36,7 @@ class ModelIndexer(object):
             logger.exception('Error deleting %s instance: %s', sender, instance)
 
     def handle_m2m_changed(self, sender, instance, action, **kwargs):
-        if action == 'post_add' or action == 'post_clear' or action == 'post_clear':
+        if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
             try:
                 index(instance)
             except:
