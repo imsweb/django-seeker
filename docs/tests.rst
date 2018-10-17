d51a0fb6ebf9d623099803042267bdd206fccd8d
Chris Mazzullo <cmazzullo@imsweb.com>
2018-10-17 10:03:02 -0400

    Updated the tests/settings.py file to make it possible to run tests
    Also updated the docs/tests.rst file to reflect the changes

--- docs/tests.rst
+++ docs/tests.rst
@@ -1,10 +1,19 @@
 Running Tests
 =============
 
-The Seeker unit tests are based on `Django's testing framework`_. The easiest way to get the tests running is to create a virtualenv_, then::
+The Seeker unit tests are based on `Django's testing framework`_.
+
+To run the tests, first you'll need to update tests/settings.py with
+the URL(s) of your elasticsearch server(s) and the username and
+password that you use to authenticate on them.
+
+After configuring your settings file, create a virtualenv_ for seeker to run in and install its dependencies with:
 
     cd tests
     pip install -r requirements.txt
+
+Finally, you should be able to run the tests with:
+
     python manage.py test
 
 .. _`Django's testing framework`: https://docs.djangoproject.com/en/1.7/topics/testing/
--- tests/settings.py
+++ tests/settings.py
@@ -1,6 +1,18 @@
 import sys
 import os
 
+# CHANGE THESE SETTINGS to work with your configuration:
+
+SEEKER_HOSTS = [
+    'http://my-elasticsearch-server-01.com:9200',
+    'http://my-elasticsearch-server-02.com:9200'
+]
+SEEKER_HTTP_AUTH = 'USERNAME:PASSWORD'
+
+
+# The settings below shouldn't need to be customized
+
+SEEKER_INDEX = 'unique-seeker-index-928374'
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Make sure the copy of seeker in the directory above this one is used.
@@ -11,6 +23,8 @@ SECRET_KEY = 'seeker_tests__this_is_not_very_secret'
 INSTALLED_APPS = (
     'seeker',
     'core',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
 )
 
 ROOT_URLCONF = 'urls'
@@ -27,5 +41,3 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-
-SEEKER_INDEX = 'seeker-tests'
