d48a70de0c17ce54060d275d39a8eed0d21f7b8c
smithl <smithl@imsweb.com>
2019-04-16 15:11:31 -0400

    Changed version to 4.4.0

--- seeker/__init__.py
+++ seeker/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '4.3.4'
+__version__ = '4.4.0'
 
 from .facets import DateRangeFacet, DateTermsFacet, Facet, GlobalTermsFacet, RangeFilter, TermsFacet, YearHistogram, TextFacet
 from .mapping import (
