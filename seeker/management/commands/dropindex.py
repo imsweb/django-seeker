ca8d0f53a8ad36fcdaac1f383571ecf76ed86b03
lefcourn <16122890+lefcourn@users.noreply.github.com>
2019-07-30 09:33:07 -0400

    Drop index (#100)
    * Setup dropindex command for seeker 6
    
    Will drop all indexes that begin with SEEKER_INDEX_PREFIX setting if set
    otherwise drop index from user input if it has auth (enables wildcard)
    may want to add in by doc class?
    
    * Updated dropindex command (based on the changes Lewis made)

--- seeker/management/commands/dropindex.py
+++ seeker/management/commands/dropindex.py
@@ -1,16 +1,17 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
 from elasticsearch_dsl.connections import connections
-
+from elasticsearch.exceptions import AuthorizationException, NotFoundError
+from django.core.exceptions import ImproperlyConfigured
 
 class Command (BaseCommand):
-    help = 'Drops the current ES index, or one that you specify.'
+    help = 'Drops all ES indexes on project with SEEKER_INDEX_PREFIX from settings, or one that you specify. To drop indexes with prefix add wildcard * after prefix of indexes you want deleted'
 
     def add_arguments(self, parser):
         parser.add_argument('--index',
             dest='index',
             default=None,
-            help='The ES index to drop'
+            help='The ES index(es) to drop'
         )
         parser.add_argument('--using',
             dest='using',
@@ -19,16 +20,29 @@ class Command (BaseCommand):
         )
 
     def handle(self, *args, **options):
-        index = options['index'] or getattr(settings, 'SEEKER_INDEX', 'seeker')
+        if options['index']:
+            index_prefix = options['index']
+        else:
+            seeker_index_prefix = getattr(settings, 'SEEKER_INDEX_PREFIX', None)
+            if seeker_index_prefix:
+                index_prefix = '{}*'.format(seeker_index_prefix)
+            else:
+                raise ImproperlyConfigured('An index or index prefix must be supplied (either through --index or SEEKER_INDEX_PREFIX setting)')
         connection = options['using'] or 'default'
         es = connections.get_connection(connection)
-
-        print 'Attempting to drop index "%s" using "%s" connection...' % (index, connection)
-        if es.indices.exists(index=index):
-            es.indices.delete(index=index)
-            if es.indices.exists(index=index):
-                print '...The index was NOT dropped.'
-            else:
-                print '...The index was dropped.'
-        else:
-            print '...The index could not be dropped because it does not exist.'
+        print('Using connection: "{}"'.format(connection))
+        print('Attempting to drop index(es) using the pattern: {}'.format(index_prefix))
+        for index in es.indices.get(index_prefix):
+            try:
+                print('Attempting to drop index "{}"...'.format(index))
+                if es.indices.exists(index=index):
+                    es.indices.delete(index=index)
+                    if es.indices.exists(index=index):
+                        print('...The index was NOT dropped.')
+                    else:
+                        print('...The index was dropped.')
+                else:
+                    print('...The index could not be dropped because it does not exist.')
+            except AuthorizationException:
+                print('You are not authorized to drop index: "{}")'.format(index_prefix))
+        print('Done. Please verify statements above for success/failure.')
