afb88ecbf7ff4acb2b4ac380c7f12a3cfd4717aa
lbrant1 <2923766+lbrant1@users.noreply.github.com>
2019-09-25 15:13:58 -0400

    fix another bug with offset
    fix bug with edge case on offset and a page value one higher than the max

--- seeker/views.py
+++ seeker/views.py
@@ -819,7 +819,7 @@ class SeekerView(View):
         page = int(page) if page.isdigit() else 1
         offset = (page - 1) * page_size
         results_count = search[0:0].execute().hits.total
-        if results_count < offset:
+        if results_count <= offset:
             page = 1
             offset = 0
 
