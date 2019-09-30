06c49064cb5349c76984302e8f32dc7e97e98ffd
Bradly Rutten <ruttenb@imsweb.com>
2019-09-30 13:35:57 -0400

    Version bumped to 6.2.2

--- seeker/__init__.py
+++ seeker/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '6.2.1'
+__version__ = '6.2.2'
 
 from .facets import DateRangeFacet, DateTermsFacet, Facet, GlobalTermsFacet, RangeFilter, TermsFacet, YearHistogram, TextFacet
 from .mapping import (
