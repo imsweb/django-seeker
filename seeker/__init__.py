2a6b23fe4b7160134c4c7c27e8b960ccc59039c6
hoskinsj <44065244+hoskinsj@users.noreply.github.com>
2019-10-28 14:11:12 -0400

    1928 Bump Version

--- seeker/__init__.py
+++ seeker/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '6.3.0'
+__version__ = '6.3.1'
 
 from .facets import DateRangeFacet, DateTermsFacet, Facet, GlobalTermsFacet, RangeFilter, TermsFacet, YearHistogram, TextFacet
 from .mapping import (
