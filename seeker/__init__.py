efdcaf5de5db7f3239524d043b5a45c87cac424d
Lewis Brant <brantl@imsweb.com>
2020-09-18 11:04:12 -0400

    bump seeker version

--- seeker/__init__.py
+++ seeker/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '6.5.2'
+__version__ = '6.5.3'
 
 from .facets import DateRangeFacet, DateTermsFacet, Facet, GlobalTermsFacet, RangeFilter, TermsFacet, YearHistogram, TextFacet
 from .mapping import (
