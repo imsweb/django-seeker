9d30812e3718ca250ac15cc40b46ef7306cd65cc
ruttenb <ruttenb@imsweb.com>
2025-06-25 10:47:54 -0400

    bump version

--- seeker/__init__.py
+++ seeker/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '7.4.4'
+__version__ = '7.5.0'
 
 from .facets import DateRangeFacet, DateTermsFacet, Facet, GlobalTermsFacet, RangeFilter, TermsFacet, YearHistogram, TextFacet, KeywordFacet
 from .mapping import (
