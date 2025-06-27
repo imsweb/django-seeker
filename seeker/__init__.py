56eb0cbd1810bc56a552e33ee796949f8e931901
ruttenb <ruttenb@imsweb.com>
2025-06-27 11:40:47 -0400

    Remove support for ElasticSearch

--- seeker/__init__.py
+++ seeker/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '7.5.0'
+__version__ = '8.0.0'
 
 from .facets import DateRangeFacet, DateTermsFacet, Facet, GlobalTermsFacet, RangeFilter, TermsFacet, YearHistogram, TextFacet, KeywordFacet
 from .mapping import (
